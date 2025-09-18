e645c01bfe3fc265fc33ea0a85c60b8a1e3b79fa
Him1002 <himanshutatamiya808@gmail.com>
2025-09-18 17:41:21 +0530

    Add Hashing

---
+++ Hashing/lowerCharHashing.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    //precompute:
+    int hash[26] = {0};
+    for(int i = 0; i<s.size(); i++){
+        hash[s[i] - 'a']++;
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        char ch;
+        cin >> ch;
+        //fetch:
+        cout << hash[ch - 'a'] << endl;
+    }
+}
+
+/*
+I/P:
+abcdabegh
+5
+a
+d
+f
+b
+z
+*/
