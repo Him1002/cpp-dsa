d2b22e463c76cb5b93682929954752c77e493a0c
Him1002 <himanshutatamiya808@gmail.com>
2025-09-20 22:13:23 +0530

    Add RecursiveBubble

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -23,15 +23,29 @@ void bubbleSort(int n, int arr[]){
     }
 }
 
+void recBubbleSort(int n,int arr[]){
+    if(n == 1) return;
+
+    for(int j = 0; j <= n-2; j++){
+        if(arr[j] > arr[j+1]){
+            int temp = arr[j+1];
+            arr[j+1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    recBubbleSort(n-1, arr);
+}
+
 int main(){
     int n;
     cin >> n;
     int arr[n];
     for(int i=0; i<n; i++) cin >> arr[i];
-    bubbleSort(n,arr);
+    recBubbleSort(n,arr);
     for(int i=0; i<n; i++){
         cout << arr[i] <<" ";
     }
+    return 0;
 }
 
 /*
@@ -43,7 +57,16 @@ I/P:
 1 2 3 4 5 6
 */
 
-/*
+/* For Bubble Sort:
 T.C. : O(n) for Worst and Avg case.
        O(1) for Best case.
+S.C. : O(1)
 */
+
+/* For Recursive Bubble Sort:
+Time Complexity: O(N^2) for the worst and average cases 
+                and O(N) for the best case. 
+                Here, N = size of the array.
+
+Space Complexity: O(N) auxiliary stack space.
+*/        
